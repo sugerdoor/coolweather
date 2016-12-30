69df9a61662be30cd4b973f31fa7098614cab23d
zhazha <1149069808@qq.com>
2016-12-30 15:37:08 +0800

    加入显示天气的功能

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -13,7 +13,10 @@ import com.coolweather.app.util.Utility;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -45,6 +48,14 @@ public class ChooseAreaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_selected", false)) {
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -64,6 +75,14 @@ public class ChooseAreaActivity extends Activity {
 				} else if (currentLevel == LEVEL_CITY) {
 					selectedCity = cityList.get(index);
 					queryCounties();
+				} else if (currentLevel == LEVEL_COUNTRY) {
+					String countryCode = countryList.get(index)
+							.getCountryCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,
+							WeatherActivity.class);
+					intent.putExtra("country_code", countryCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
@@ -179,7 +198,8 @@ public class ChooseAreaActivity extends Activity {
 				runOnUiThread(new Runnable() {
 					public void run() {
 						closeProgressDialog();
-						Toast.makeText(ChooseAreaActivity.this, "º”‘ÿ ß∞‹", Toast.LENGTH_SHORT).show();
+						Toast.makeText(ChooseAreaActivity.this, "º”‘ÿ ß∞‹",
+								Toast.LENGTH_SHORT).show();
 					}
 				});
 			}
--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -1,5 +1,15 @@
 package com.coolweather.app.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.coolweather.app.model.City;
@@ -8,6 +18,46 @@ import com.coolweather.app.model.Country;
 import com.coolweather.app.model.Province;
 
 public class Utility {
+	/**
+	 * 解析服务器返回的JSON数据，并将解析出的数据存储到本地。
+	 * */
+	public static void handleWeatherResponse(Context context, String response) {
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+					weatherDesp, publishTime);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 将服务器返回的所有天气信息存储到SharedPreference文件中。
+	 * */
+	public static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager
+				.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
+	}	
+
 	/**
 	 * 解析和处理服务器返回的省级级数据
 	 */
@@ -21,22 +71,23 @@ public class Utility {
 					Province province = new Province();
 					province.setProvinceCode(array[0]);
 					province.setProvinceName(array[1]);
-					//将解析出来的数据存储到City表
+					// 将解析出来的数据存储到City表
 					coolWeatherDB.saveProvince(province);
-					
+
 				}
 				return true;
-				
+
 			}
 		}
 		return false;
 
 	}
+
 	/**
 	 * 解析和处理服务器返回的市级级数据
 	 */
 	public synchronized static boolean handleCitiesResponse(
-			CoolWeatherDB coolWeatherDB, String response,int provinceId) {
+			CoolWeatherDB coolWeatherDB, String response, int provinceId) {
 		if (!TextUtils.isEmpty(response)) {
 			String[] allCities = response.split(",");
 			if (allCities != null && allCities.length > 0) {
@@ -46,39 +97,41 @@ public class Utility {
 					city.setCityCode(array[0]);
 					city.setCityName(array[1]);
 					city.setProvinceId(provinceId);
-					//将解析出来的数据存储到City表
+					// 将解析出来的数据存储到City表
 					coolWeatherDB.saveCity(city);
-					
+
 				}
 				return true;
-				
+
 			}
 		}
 		return false;
 
 	}
+
 	/**
 	 * 解析和处理服务器返回的县级数据
 	 */
-	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,String response,int cityId){
+	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int cityId) {
 		if (!TextUtils.isEmpty(response)) {
 			String[] allcounties = response.split(",");
-			if (allcounties != null && allcounties.length>0) {
+			if (allcounties != null && allcounties.length > 0) {
 				for (String c : allcounties) {
 					String[] array = c.split("\\|");
 					Country country = new Country();
 					country.setCountryCode(array[0]);
 					country.setCountryName(array[1]);
 					country.setCityId(cityId);
-					//将解析出来的数据存储到county表
+					// 将解析出来的数据存储到county表
 					coolWeatherDB.saveCountry(country);
 				}
 				return true;
-				
+
 			}
 		}
-		
+
 		return false;
-		
+
 	}
 }
